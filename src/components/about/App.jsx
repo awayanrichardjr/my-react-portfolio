6e63e9aa7e15b64933e371d835b61f1b35217033
Richard Awayan <awayanrichardjr@gmail.com>
2025-09-08 08:27:20 +0800

    adding expertise

--- src/components/about/App.jsx
+++ src/components/about/App.jsx
@@ -1,10 +1,35 @@
 import "./App.css";
+import profilePic from "../assets/img/me/me.jpg";
 
 function App() {
   return (
     <>
       <section className="about" id="about">
         <h1>About me</h1>
+        <div className="container">
+          <img src={profilePic} alt="profile-pic" />
+          <div className="profile-name">
+            <h1>Richard Dumael Awayan Jr.</h1>
+            <p>Frontend Developer</p>
+            <br />
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Suscipit, laborum maxime dolores repellat quasi ipsa quae quos
+              odio inventore culpa nam fugit quaerat ducimus nihil officiis
+              obcaecati! Dicta, molestias sapiente?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam,
+              incidunt.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia
+              reprehenderit ipsum, dicta culpa amet error reiciendis pariatur
+              saepe excepturi, nulla vitae, vel tempora quis. Minus repellat
+              excepturi ipsam esse id.
+            </p>
+          </div>
+        </div>
       </section>
     </>
   );
--- src/components/expertise/App.jsx
+++ src/components/expertise/App.jsx
@@ -4,7 +4,104 @@ function App() {
   return (
     <>
       <section className="expertise" id="expertise">
-        <h1>Expertise</h1>
+        <div className="container">
+          {/* table */}
+          <table>
+            {/* table head */}
+            <thead>
+              <tr>
+                <th>
+                  <h1>Expertise</h1>
+                </th>
+              </tr>
+            </thead>
+
+            {/* table body */}
+            <tbody>
+              <tr>
+                <td>
+                  <h1>Core Website Development</h1>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Itaque, eos.
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h1>Web Application Development</h1>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Itaque, eos.
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h1>UI/UX Implementation</h1>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Itaque, eos.
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h1>API Integration</h1>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Itaque, eos.
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h1>Optimization & Maintenance</h1>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Itaque, eos.
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h1>Accessibility & Compliance</h1>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Itaque, eos.
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h1>E-commerce Development</h1>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Itaque, eos.
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h1>Testing & Quality Assurance</h1>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Itaque, eos.
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h1>Deployment & Support</h1>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Itaque, eos.
+                  </p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </section>
     </>
   );
