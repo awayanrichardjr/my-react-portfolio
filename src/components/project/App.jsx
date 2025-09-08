05f5bc4f5da691eaf3cd6227cba6c45d2b81ce3f
Richard Awayan <awayanrichardjr@gmail.com>
2025-09-08 08:39:38 +0800

    finishing layout

--- src/components/project/App.jsx
+++ src/components/project/App.jsx
@@ -4,7 +4,27 @@ function App() {
   return (
     <>
       <section className="project" id="project">
-        <h1>Projects</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                <h1>Projects</h1>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <h1>Telehealth Access</h1>
+                <p>
+                  An Online Consultation and Appointment System with Predictive
+                  Analytics
+                </p>
+                <a href="#">link</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </section>
     </>
   );
