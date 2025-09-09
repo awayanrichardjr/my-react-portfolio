17ae112665fd1c192f4b562d71a0a905b0f7e810
Richard Awayan <awayanrichardjr@gmail.com>
2025-09-09 20:18:54 +0800

    update a link of email

--- src/components/contact/App.jsx
+++ src/components/contact/App.jsx
@@ -37,7 +37,13 @@ function App() {
               <span className="material-symbols-rounded">email</span>
               <p>
                 <address>
-                  <a href="mailto:awayanrichardjr@gmail.com?subject=Hello?body=Hardric%20at%20your%20service.%20Message%20here...?cc=richarddumaelawayanjr@gmail.com">
+                  {/**
+                   *
+                   *
+                   *
+                   * a link that the sender can send n email message to the receiver.
+                   *  */}
+                  <a href="mailto:awayanrichardjr@gmail.com??cc=richarddumaelawayanjr@gmail.comsubject=Hello?body=Hardric%20at%20your%20service.%20Message%20here...">
                     awayanrichardjr@gmail.com
                   </a>
                 </address>
